d31828b6666dfb251dcf7c43c21a73f2d5faa51d
PR <piotr.rybka87@outlook.com>
2020-06-05 16:23:33 +0200

    Transformation

--- game/main.cpp
+++ game/main.cpp
@@ -1,6 +1,10 @@
 #include <windows.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 using namespace std;
@@ -83,7 +87,7 @@ int main()
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // CORE dlatego potrzebuje VAO
 
     // glfw window creation
     // --------------------
@@ -104,28 +108,48 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    unsigned int VBO, VAO, EBO;
+//    float vertices[6] = {
+//        -0.5f, -0.5f, //0
+//         0.5f, -0.5f, //1
+//         0.0f,  0.5f, //2
+//    };
 
-    unsigned int VBO, VAO;
-    cout << "Triangle Init "<<endl;
-    float vertices[6] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.0f,  0.5f,
+
+
+    float vertices[8] = {
+         0.5f,  0.5f, //0
+         0.5f, -0.5f, //1
+        -0.5f, -0.5f, //2
+        -0.5f,  0.5f, //3
+    };
+
+
+    unsigned int indicesElement[6] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1,&VBO);
     glBindVertexArray(VAO);
 
+    glGenBuffers(1,&VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indicesElement, GL_STATIC_DRAW);
 
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2 ,0);
     glEnableVertexAttribArray(0);
 
+    std::cout<<"VAO: "<<VAO<<" VBO: "<<VBO<< " EBO: "<<EBO<<std::endl;
 
 
+
+    // -------------------- SHADERS --------------------
     std::string vertexShaderSourceS = "#version 330 core\n"
         "layout (location = 0) in vec4 aPos;\n"
         "void main()\n"
@@ -140,11 +164,30 @@ int main()
         "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
         "}\0";
 
+    std::string uniformVertexFragmentSourceS = "#version 330 core\n"
+       "out vec4 FragColor;\n"
+       "uniform vec4 u_Color;"
+       "void main()\n"
+       "{\n"
+       "   FragColor = u_Color;\n"
+       "}\0";
+
+    std::string transformationVertexShaderSourceS = "#version 330 core\n"
+        "layout (location = 0) in vec4 aPos;\n"
+        "uniform mat4 u_Transform;"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = u_Transform * aPos;\n"
+        "}\0";
+    // --------------------------------------------------
+
     ShaderWrapper ShaderObj;
-    ShaderObj.CreateShader(vertexShaderSourceS,vertexFragmentSourceS);
+    ShaderObj.CreateShader(transformationVertexShaderSourceS,uniformVertexFragmentSourceS);
     ShaderObj.CreateProgram();
     glUseProgram(ShaderObj.GetProgramId());
 
+
+    float greenColor = 0.01f;
 
     // render loop
     // -----------
@@ -158,10 +201,34 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
+
+        // create transformations
+        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+
         glUseProgram(ShaderObj.GetProgramId());
+
+        unsigned int transformLoc = glGetUniformLocation(ShaderObj.GetProgramId(), "u_Transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        int uniformLocation = glGetUniformLocation(ShaderObj.GetProgramId(), "u_Color");
+        glUniform4f(uniformLocation, 0.0f, greenColor, 0.8f, 1.0f);
+
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        if (greenColor > 1.0f)
+        {
+            greenColor = 0.01f;
+        }
+        else
+        {
+            greenColor += 0.01f;
+        }
+
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -169,6 +236,9 @@ int main()
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
